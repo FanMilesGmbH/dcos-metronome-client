60f7de5cfbf5367002349518d068d089a7d648d1
Pavlo Bogomolenko <pavlo.bogomolenko@fanmiles.com>
2017-12-20 16:05:04 +0100

    Use fanmiles eslint

--- test/integration/metronomeClient.js
+++ test/integration/metronomeClient.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const Promise = require('bluebird');
 
 const expect = require('./../chai').expect;
@@ -8,105 +6,104 @@ const metronomeBaseUrl = 'http://metronome.mesos:9000';
 const sut = require('../../index.js').getInstance(metronomeBaseUrl);
 
 function getRandomJobName() {
-    return 'test-' + (new Date()).getTime();
+  return `test-${(new Date()).getTime()}`;
 }
 
 describe('Metronome client (integration)', () => {
-    let jobPayload;
+  let jobPayload;
 
-    afterEach(Promise.coroutine(function * () {
-        yield sut.removeJob(jobPayload);
+  afterEach(Promise.coroutine(function* beforeEachHandler() {
+    yield sut.removeJob(jobPayload);
+  }));
+
+  context('Create a new job', () => {
+    let result;
+
+    beforeEach(Promise.coroutine(function* beforeEachHandler() {
+      jobPayload = {
+        id: getRandomJobName(),
+        run: {
+          env: {
+            TEST1: 'test',
+            TEST: 'test',
+          },
+          artifacts: [{
+            uri: 'file:///home/core/docker.tar.gz',
+            extract: true,
+            executable: false,
+            cache: false,
+          }],
+          cpus: 0.01,
+          mem: 32,
+          disk: 0,
+          docker: {
+            image: 'busybox',
+          },
+        },
+      };
+
+      result = yield sut.createJob(jobPayload);
     }));
 
-    context('Create a new job', () => {
-        let result;
+    it('should contain job id', () => expect(result).to.have.property('id'));
+  });
 
-        beforeEach(Promise.coroutine(function * () {
-            jobPayload = {
-                id: getRandomJobName(),
-                run: {
-                    env: {
-                        TEST1: 'test',
-                        TEST: 'test'
-                    },
-                    artifacts: [{
-                        uri: 'file:///home/core/docker.tar.gz',
-                        extract: true,
-                        executable: false,
-                        cache: false
-                    }],
-                    cpus: 0.01,
-                    mem: 32,
-                    disk: 0,
-                    docker: {
-                        image: 'busybox'
-                    }
-                }
-            };
+  context('Run an existing job', () => {
+    let result;
+    let inputPayload;
 
-            result = yield sut.createJob(jobPayload);
-        }));
+    beforeEach(Promise.coroutine(function* beforeEachHandler() {
+      jobPayload = {
+        id: getRandomJobName(),
+        run: {
+          cpus: 0.01,
+          mem: 32,
+          disk: 0,
+          docker: {
+            image: 'busybox',
+          },
+        },
+      };
 
-        it('should contain job id', () => expect(result).to.have.property('id'));
+      yield sut.createJob(jobPayload);
+
+      inputPayload = {
+        id: jobPayload.id,
+      };
+
+      result = yield sut.runJob(inputPayload);
+    }));
+
+    it('should contain id', () => expect(result).to.have.property('id'));
+    it('should contain job id', () => expect(result).to.have.property('jobId'));
+  });
+
+  context('Find existing jobs', () => {
+    const jobName = getRandomJobName();
+    let result;
+
+    beforeEach(Promise.coroutine(function* beforeEachHandler() {
+      jobPayload = {
+        id: jobName,
+        run: {
+          cpus: 0.01,
+          mem: 32,
+          disk: 0,
+          docker: {
+            image: 'busybox',
+          },
+        },
+      };
+
+      yield sut.createJob(jobPayload);
+
+      result = yield sut.listJobs();
+    }));
+
+    it(`should contain ${jobName} in list`, () => {
+      const createdJob = result.filter(job => job.id === jobName);
+
+      expect(createdJob).to.have.length(1);
     });
-
-    context('Run an existing job', () => {
-        let result;
-        let inputPayload;
-
-        beforeEach(Promise.coroutine(function * () {
-            jobPayload = {
-                id: getRandomJobName(),
-                run: {
-                    cpus: 0.01,
-                    mem: 32,
-                    disk: 0,
-                    docker: {
-                        image: 'busybox'
-                    }
-                }
-            };
-
-            yield sut.createJob(jobPayload);
-
-            inputPayload = {
-                id: jobPayload.id
-            };
-
-            result = yield sut.runJob(inputPayload);
-        }));
-
-        it('should contain id', () => expect(result).to.have.property('id'));
-        it('should contain job id', () => expect(result).to.have.property('jobId'));
-    });
-
-    context('Find existing jobs', () => {
-        let result;
-        let jobName = getRandomJobName();
-
-        beforeEach(Promise.coroutine(function * () {
-            jobPayload = {
-                id: jobName,
-                run: {
-                    cpus: 0.01,
-                    mem: 32,
-                    disk: 0,
-                    docker: {
-                        image: 'busybox'
-                    }
-                }
-            };
-
-            yield sut.createJob(jobPayload);
-
-            result = yield sut.listJobs();
-        }));
-
-        it('should contain '+ jobName + ' in list', () => {
-            const createdJob = result.filter(job => job.id === jobName);
-
-            expect(createdJob).to.have.length(1);
-        });
-    });
-
+  });
 });
--- test/unit/metronomeClient.js
+++ test/unit/metronomeClient.js
@@ -1,137 +1,131 @@
-'use strict';
-
 const expect = require('./../chai').expect;
-
 const Promise = require('bluebird');
 const sinonAsPromised = require('sinon-as-promised')(Promise);
 
 const MetronomeClient = require('../../MetronomeClient.js');
 
 describe('Metronome client', () => {
-    let sut;
-    let httpClient;
-    let metronomeBaseUrl;
+  let sut;
+  let httpClient;
+  let metronomeBaseUrl;
 
-    beforeEach(() => {
-        metronomeBaseUrl = 'http://metronome.mesos:9000';
+  beforeEach(() => {
+    metronomeBaseUrl = 'http://metronome.mesos:9000';
 
-        httpClient = sinonAsPromised.stub();
+    httpClient = sinonAsPromised.stub();
 
-        httpClient.resolves({
-            body: {
-                id: 'somekind-id',
-                jobId: 'someJobId'
-            }
-        });
-
-        sut = new MetronomeClient({
-            metronomeBaseUrl,
-            httpClient
-        });
+    httpClient.resolves({
+      body: {
+        id: 'somekind-id',
+        jobId: 'someJobId',
+      },
     });
 
-    context('Create a new job', () => {
-        let result;
-        let createJobPayload = {
-            id: 'hello-sleep-600-3',
-            run: {
-                cpus: 0.01,
-                mem: 32,
-                disk: 0,
-                docker: {
-                    image: 'busybox'
-                }
-            }
-        };
-
-        beforeEach(Promise.coroutine(function * () {
-            result = yield sut.createJob(createJobPayload);
-        }));
-
-        it('should contain job id', () => expect(result).to.have.property('id'));
-
-        it('should call http client exactly with', () => {
-            expect(httpClient).to.be.calledWithExactly({
-                method: 'POST',
-                json: true,
-                url: `${metronomeBaseUrl}/v1/jobs`,
-                body: createJobPayload
-            });
-        });
+    sut = new MetronomeClient({
+      metronomeBaseUrl,
+      httpClient,
     });
+  });
 
-    context('Run an existing job', () => {
-        let result;
-        let inputPayload;
+  context('Create a new job', () => {
+    let result;
+    const createJobPayload = {
+      id: 'hello-sleep-600-3',
+      run: {
+        cpus: 0.01,
+        mem: 32,
+        disk: 0,
+        docker: {
+          image: 'busybox',
+        },
+      },
+    };
 
-        beforeEach(Promise.coroutine(function * () {
-            inputPayload = {
-                id: 'hello-sleep-600-3'
-            };
-            result = yield sut.runJob(inputPayload);
-        }));
+    beforeEach(Promise.coroutine(function* beforeEachHandler() {
+      result = yield sut.createJob(createJobPayload);
+    }));
 
-        it('should contain id', () => expect(result).to.have.property('id'));
-        it('should contain job id', () => expect(result).to.have.property('jobId'));
+    it('should contain job id', () => expect(result).to.have.property('id'));
 
-        it('should call http client exactly with', () => {
-            expect(httpClient).to.be.calledWithExactly({
-                method: 'POST',
-                json: true,
-                url: `${metronomeBaseUrl}/v1/jobs/${inputPayload.id}/runs`
-            });
-        });
+    it('should call http client exactly with', () => {
+      expect(httpClient).to.be.calledWithExactly({
+        method: 'POST',
+        json: true,
+        url: `${metronomeBaseUrl}/v1/jobs`,
+        body: createJobPayload,
+      });
     });
+  });
 
-    context('Remove an existing job', () => {
+  context('Run an existing job', () => {
+    let result;
+    let inputPayload;
 
-        let inputPayload;
+    beforeEach(Promise.coroutine(function* beforeEachHandler() {
+      inputPayload = {
+        id: 'hello-sleep-600-3',
+      };
+      result = yield sut.runJob(inputPayload);
+    }));
 
-        beforeEach(Promise.coroutine(function * () {
-            inputPayload = {
-                id: 'hello-sleep-600-3'
-            };
-            yield sut.removeJob(inputPayload);
-        }));
+    it('should contain id', () => expect(result).to.have.property('id'));
+    it('should contain job id', () => expect(result).to.have.property('jobId'));
 
-        it('should call http client exactly with', () => {
-            expect(httpClient).to.be.calledWithExactly({
-                method: 'DELETE',
-                json: true,
-                url: `${metronomeBaseUrl}/v1/jobs/${inputPayload.id}?stopCurrentJobRuns=true`
-            });
-        });
+    it('should call http client exactly with', () => {
+      expect(httpClient).to.be.calledWithExactly({
+        method: 'POST',
+        json: true,
+        url: `${metronomeBaseUrl}/v1/jobs/${inputPayload.id}/runs`,
+      });
     });
+  });
 
-    context('Find jobs', () => {
+  context('Remove an existing job', () => {
+    let inputPayload;
 
-        beforeEach(Promise.coroutine(function * () {
-            yield sut.listJobs();
-        }));
+    beforeEach(Promise.coroutine(function* beforeEachHandler() {
+      inputPayload = {
+        id: 'hello-sleep-600-3',
+      };
+      yield sut.removeJob(inputPayload);
+    }));
 
-        it('should call http client exactly with', () => {
-            expect(httpClient).to.be.calledWithExactly({
-                method: 'GET',
-                json: true,
-                url: `${metronomeBaseUrl}/v1/jobs`
-            });
-        });
+    it('should call http client exactly with', () => {
+      expect(httpClient).to.be.calledWithExactly({
+        method: 'DELETE',
+        json: true,
+        url: `${metronomeBaseUrl}/v1/jobs/${inputPayload.id}?stopCurrentJobRuns=true`,
+      });
     });
+  });
 
-    context('Find jobs with argument "history"', () => {
+  context('Find jobs', () => {
+    beforeEach(Promise.coroutine(function* beforeEachHandler() {
+      yield sut.listJobs();
+    }));
 
-        const argument = 'history';
-
-        beforeEach(Promise.coroutine(function * () {
-            yield sut.listJobs(argument);
-        }));
-
-        it('should call http client exactly with', () => {
-            expect(httpClient).to.be.calledWithExactly({
-                method: 'GET',
-                json: true,
-                url: `${metronomeBaseUrl}/v1/jobs?embed=${argument}`
-            });
-        });
+    it('should call http client exactly with', () => {
+      expect(httpClient).to.be.calledWithExactly({
+        method: 'GET',
+        json: true,
+        url: `${metronomeBaseUrl}/v1/jobs`,
+      });
     });
+  });
+
+  context('Find jobs with argument "history"', () => {
+    const argument = 'history';
+
+    beforeEach(Promise.coroutine(function* beforeEachHandler() {
+      yield sut.listJobs(argument);
+    }));
+
+    it('should call http client exactly with', () => {
+      expect(httpClient).to.be.calledWithExactly({
+        method: 'GET',
+        json: true,
+        url: `${metronomeBaseUrl}/v1/jobs?embed=${argument}`,
+      });
+    });
+  });
 });
